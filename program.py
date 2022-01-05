26687b81392e7c94a6d60c6440df0137b18da7f3
LucilleNU <nawalucille@gmail.com>
2022-01-05 20:17:20 +0200

    update after improvement

--- program.py
+++ program.py
@@ -2,7 +2,58 @@
 import unittest
 import math
 
-def fab(n):
+def fab(n):"Testing Absolute"
+
+import unittest
+import math
+
+
+def fab(num):
+    """
+    Wrapper Function
+    """
+    if not num:
+        raise TypeError("Invalid input")
+    if not isinstance(num, int):
+        raise TypeError("Integer Only")
+    return math.fabs(num)
+
+
+class TestMathFunc(unittest.TestCase):
+    "Test Math Function"
+
+    def test_right_parameter(self):
+        "Right Parameter Testing"
+        input_r = -2.22
+        desired = 2.22
+        self.assertEqual(math.fabs(input_r), desired)
+
+    def test_positive_parameter(self):
+        "Positive Number Testing"
+        input_r = 2.2
+        desired = 2.2
+        self.assertEqual(math.fabs(input_r), desired)
+
+    def test_wrong_param(self):
+        "Wrong Parameter Testing"
+        with self.assertRaises(TypeError):
+            fab({})
+        with self.assertRaises(TypeError):
+            fab([])
+        with self.assertRaises(TypeError):
+            fab("Invalid")
+
+    def test_empty_parameter(self):
+        "Empty Parameter Testing"
+        with self.assertRaises(TypeError):
+            self.assertEqual(fab(), True)
+        with self.assertRaises(TypeError):
+            self.assertEqual(fab(''), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
     if not n:
         raise TypeError("Invalid input")
     if not isinstance(n, int):
