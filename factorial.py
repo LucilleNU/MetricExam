bf4836d5209a1150f14f91ce998e572860eef188
LucilleNU <nawalucille@gmail.com>
2022-01-05 20:24:20 +0200

    update after improvement

--- factorial.py
+++ factorial.py
@@ -2,6 +2,51 @@
 import unittest
 import math
 
+"Testing Factorial"
+
+import unittest
+import math
+
+
+def factorial_func(num):
+    'Factorial Function Validation'
+    if not num:
+        raise TypeError("Invalid Input")
+    if num < 0:
+        raise ValueError('Only Non Negative')
+    return math.factorial(num)
+
+
+class TestMathFactorial(unittest.TestCase):
+    "Class Testing Factorial"
+
+    def test_right_parameter(self):
+        "Right Parameter Test"
+        input_r = 5
+        desired = 120
+        self.assertEqual(math.factorial(input_r), desired)
+        self.assertEqual(factorial_func(input_r), desired)
+
+    def test_empty_parameter(self):
+        "Empty Parameter Test"
+        with self.assertRaises(TypeError):
+            self.assertEqual(factorial_func(), True)
+        with self.assertRaises(TypeError):
+            self.assertEqual(factorial_func(''), True)
+
+    def test_negative_parameter(self):
+        "Testing Negative Parameter"
+        with self.assertRaises(ValueError):
+            factorial_func(-5)
+
+    def test_invalid_parameter(self):
+        "Invalid Parameter Test"
+        with self.assertRaises(TypeError):
+            factorial_func()
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 def factorial_func(x):
     'Factorial Function Validation'
